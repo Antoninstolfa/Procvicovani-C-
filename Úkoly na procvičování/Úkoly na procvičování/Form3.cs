df9a2e153c10ce3a5d491a366772126a7536f82a
Antonín Štolfa <stolfaa.04@spst.eu>
2022-05-23 21:13:48 +0200

    Hotovo

--- Úkoly na procvičování/Úkoly na procvičování/Form3.cs
+++ Úkoly na procvičování/Úkoly na procvičování/Form3.cs
@@ -47,7 +47,56 @@ namespace Úkoly_na_procvičování
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                int pocitadloL = 0, MinCislo = int.MaxValue;
+                int soucin = 1, MaxCislo = int.MinValue;
+                int cifernySoucetMax = 0;
+                int[] pole = new int[textBox1.Lines.Count()];
+                for (int i = 0; i < textBox1.Lines.Count(); i++)
+                {
+                    pole[i] = Convert.ToInt32(textBox1.Lines[i]);
+                }
+                foreach (int cislo in pole)
+                {
+                    if (cislo % 2 != 0)
+                    {
+                        pocitadloL++;
+                    }
+                    if (cislo < MinCislo)
+                    {
+                        MinCislo = cislo;
+                    }
+                    if (cislo >= 5 && cislo < 20)
+                    {
+                        soucin *= cislo;
+                    }
+                    if (cislo > MaxCislo)
+                    {
+                        MaxCislo = cislo;
+                    }
+                }
+                while (MaxCislo != 0)
+                {
+                    cifernySoucetMax += MaxCislo % 10;
+                    MaxCislo = MaxCislo / 10;
+                }
+                PocetLichychLabel.Text = pocitadloL.ToString();
+                MinCisloLabel.Text = MinCislo.ToString();
+                if (soucin > 1)
+                {
+                    soucinCiselLabel.Text = soucin.ToString();
+                }
+                else
+                {
+                    soucinCiselLabel.Text = "0";
+                }
+                cifernySoucetMaxLabel.Text = cifernySoucetMax.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Zadejte pouze čísla!", "Error",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } 
         }
     }
 }
